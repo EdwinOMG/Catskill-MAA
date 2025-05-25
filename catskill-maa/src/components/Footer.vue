edf12f4a0c97e3207336772bc0bff90fcb3a6fa6
EdwinOMG <150313733+EdwinOMG@users.noreply.github.com>
2025-05-24 21:45:18 -0400

    fixed the footer and moved logo to public/

--- catskill-maa/src/components/Footer.vue
+++ catskill-maa/src/components/Footer.vue
@@ -3,37 +3,77 @@
 </script>
 
 <template>
-    <footer class="footer py-0   ">
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+/>
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+/>
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bulma-social@2/css/all.min.css"
+/>
+    <footer class="footer has-background-light-blue has-text-black">
         <div class="container">
-            <h2 class="title has-text-centered is-2 mb-2 ">Start your journey today!</h2>
-
-            <div class="columns is-variable is-6 is-multiline has-text-centered-mobile">
-                <div class="column is-half-tablet is-one-third-desktop">
-                    <h3 class="title is-5">Address</h3>
-                    <p class="subtitle is-6">1175 NY-52<br />Loch Sheldrake, NY 12759</p>
+            <div class="columns is-multiline">
+                <div class="column is-3 has-text-centered">
+                    <figure class="image is-inline-block">
+                        <img src="/logo.jpg" alt="Catskill Martial Arts Academy Logo" />
+                    </figure>
+                    <p class="has-text-white mt-2">Catskill Martial Arts Academy</p>
                 </div>
 
-                <div class="column is-half-tablet is-one-third-desktop">
-                    <h3 class="title is-5">Contact</h3>
-                    <p class="subtitle is-6">Phone: (845) 423-949</p>
-                    <p class="subtitle is-6"></p>
+                <div class="column is-2">
+                    <h3 class="title is-5 has-text-black">Hours</h3>
+                    <p>Sunday: Closed</p>
+                    <p>Monday: 6PM-9PM</p>
+                    <p>Tuesday: 7PM-9PM</p>
+                    <p>Wednesday: 6PM-9PM</p>
+                    <p>Thursday: 7PM-9PM</p>
+                    <p>Friday: 6PM-9PM</p>
+                    <p>Saturday: 10AM-1PM</p>
+                </div>
+                <div class="column is-1.5">
+                    <h3 class="title is-5 has-text-black">Navigation</h3>
+                    <ul>
+                        <li><a href="#" class="has-text-blue">Home</a></li>
+                        <li><a href="#" class="has-text-blue">About</a></li>
+                        <li><a href="#" class="has-text-blue">Classes</a></li>
+                        <li><a href="#" class="has-text-blue">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div class="column is-3">
+                    <h3 class="title is-5 has-text-black">Contact</h3>
+                    <p class="has-text-black">Email: catskillmaa@yahoo.com</p>
+                    <p class="has-text-black">Phone: (123) 456-7890</p>
+                    <p class="has-text-black">address here womp womp</p>
+                </div>
+                <div class="column is-2">
+                    <h3 class="title is-5 has-text-black">Follow us</h3>
+                    <div class="buttons">
+                        <a class="button is-facebook" href="https://www.facebook.com/catskillmaa/">
+                            <span class="icon">
+                                <i class="fab fa-facebook"></i>
+                            </span>
+                        </a>
+                        <a class="button is-link" href="https://www.instagram.com/catskillmaa/">
+                            <span class="icon">
+                                <i class="fab fa-instagram"></i>
+                            </span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </footer >
+        <div class="content has-text-centered">
+            <p class="has-text-white">© 2023 Catskill Martial Arts Academy. All rights reserved.</p>
+        </div>
+    </footer>
 </template>
 
-<style scoped lang="scss">
-@use '../../catskill-maa.scss' as *;
-    .footer {
-        background-color: $white;
-        margin-top: 0px;   
-        height: 300px;     
-    }
-    .title {
-        color:black;
-    }
-    .subtitle {
-        color: darkblue;
-    }
+<style scoped>
+
 </style>
