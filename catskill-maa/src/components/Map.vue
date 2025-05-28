10223adfba1b6d208c367744df6b06c0d7c3f17a
EdwinOMG <150313733+EdwinOMG@users.noreply.github.com>
2025-05-28 02:06:36 -0400

    death

--- catskill-maa/src/components/Map.vue
+++ catskill-maa/src/components/Map.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { GoogleMap, Marker } from 'vue3-google-map'
-const apiKey = import.meta.env.VUE_APP_API_KEY;
+const APIKEY="AIzaSyB9bvPl80g9ntUKtsuTnKF0E1o730i1JPI"
+// leaving ts for now im lazy, please dont run up my bill...
 const center =  { lat: 41.77097496721765, lng: -74.65850133820045}
 </script>
 
 <template>
     <GoogleMap 
-    :api-key="apiKey"
+    :api-key="APIKEY"
     style="width: 100%; height: 300px;"
     :center="center"
     :zoom="15"
@@ -16,5 +17,7 @@ const center =  { lat: 41.77097496721765, lng: -74.65850133820045}
 </template>
 
 <style scoped>
-
+.google-map {
+    padding-top: 120px;
+}
 </style>
