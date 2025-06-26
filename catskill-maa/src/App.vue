7f1d28193b1242c6b0cfb5436b752d5240daa919
EdwinOMG <150313733+EdwinOMG@users.noreply.github.com>
2025-06-26 12:01:31 -0400

    added top bar with contact info

--- catskill-maa/src/App.vue
+++ catskill-maa/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
   import Navbar from './components/Navbar.vue'
   import Footer from './components/Footer.vue';
+  import Topbar from './components/Topbar.vue';
 </script>
 
 <template>
   <div id="app">
   <v-app>
     <header>
+    <Topbar />
     <Navbar />
     </header>
     <RouterView />
---
+++ catskill-maa/src/components/Topbar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="top-bar">
+       Call us: (845) xxx-xxxx  | Email: catskillmaa@yahoo.com
+    </div>
+</template>
+
+<style scoped>
+.top-bar {
+  background-color: gray;
+  color: #fff;
+  font-size: 0.8rem;
+  padding: 0.3rem 1rem;
+  text-align: center;
+  font-weight: 500;
+}
+</style>
